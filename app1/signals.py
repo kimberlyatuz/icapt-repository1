49bc64052cc5089ffb8b899c7e27e2ad1bb03ee0
kimberlyatuz <kimberlyatuz@gmail.com>
2025-03-31 23:57:28 +0200

    signals.py

--- app1/signals.py
+++ app1/signals.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User,Group
 from .models import UserProfile
+from django.contrib.auth import get_user_model
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -15,17 +16,18 @@ def save_user_profile(sender, instance, **kwargs):
     # Create profile if it doesn't exist
     UserProfile.objects.get_or_create(user=instance)
 
+User = get_user_model()
+
 @receiver(post_save, sender=User)
 def add_to_staff_group(sender, instance, created, **kwargs):
     if instance.is_staff:
-        staff_group = Group.objects.get(name='staff')
+        staff_group, _ = Group.objects.get_or_create(name='staff')  # Safe get/create
         instance.groups.add(staff_group)
 
-     #TO AUTOMATICALLY ADD ADMIN USERS TO THE ADMIN GROUP
 @receiver(post_save, sender=User)
 def handle_admin_group(sender, instance, **kwargs):
     """Sync admin group with is_staff status"""
-    admin_group = Group.objects.get(name='admin')
+    admin_group, _ = Group.objects.get_or_create(name='admin')  # Safe get/create
     if instance.is_staff:
         instance.groups.add(admin_group)
     else:
