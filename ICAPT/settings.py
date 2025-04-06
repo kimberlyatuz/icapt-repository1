6b81115a3dd00f58dc4f92fd3534672a17c1ed3e
kimberlyatuz <kimberlyatuz@gmail.com>
2025-04-06 09:44:55 +0200

    Update settings.py

--- ICAPT/settings.py
+++ ICAPT/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'app1.middleware.GroupAuthMiddleware',
+    'app1.middleware.GroupAuthMiddleware',
     
 ]
 
