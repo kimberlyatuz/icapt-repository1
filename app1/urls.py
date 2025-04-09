a2f0b5d7ddbb206ab8ad170cc401e66bfdf7c3b2
kimberlyatuz <kimberlyatuz@gmail.com>
2025-04-09 12:09:21 +0200

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
-
+from .views import process_document
 from . import views
 from .views import (review_submissions, ReviewRecords, \
-    import_data, edit_submission, dashboard, submissions_list, user_submissions, update_submission, \
-    bulk_accept_submissions, bulk_reject_submissions, unauthorized, ME_Dashboard,bulk_delete, get_patient_by_referral, patient_search)
-from django.contrib.auth.views import LoginView, LogoutView
+    import_data, form_view, add_patient, edit_submission, dashboard, submissions_list, user_submissions, update_submission, \
+    bulk_accept_submissions, LoginView, bulk_reject_submissions, unauthorized, ME_Dashboard,bulk_delete, fetch_patient, patient_search, PasswordResetView)
 
 
 urlpatterns = [
 # Authentication
     path('login/', views.loginpage, name='login'),
+    path('register/', views.register,name='register'),
     path('logout/', views.logoutuser, name='logout'),
-    path('register/', views.register, name='register'),
-    path('forgot_password', views.forgot_password, name='forgot_password'),
-    path('reset_password', views.reset_password, name='reset_password'),
-
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('password-reset/', views.PasswordResetView, name='password_reset'),
+    path('password-reset/done/', views.PasswordResetDoneView, name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView, name='password_reset_confirm'),
+    path('password-reset-complete/', views.PasswordResetCompleteView,name='password_reset_complete'),
     # Main pages
     path('', views.index, name='index'),
     path('unauthorized/', unauthorized, name='unauthorized'),
@@ -30,16 +31,20 @@ urlpatterns = [
     path('user_forms_list/', views.user_forms_list, name='user_forms_list'),
     path('delete_user_form/<int:form_id>/', views.delete_user_form, name='delete_user_form'),
     path('form_fields_list/<int:form_id>/', views.form_fields_list, name='form_fields_list'),
-    path('form_view/<int:form_id>/', views.form_view, name='form_view'),
+    path('process-document/', process_document, name='process_document'),
+    path('form/<int:form_id>/', form_view, name='form_view'),
 
     path('fill_form/<int:form_id>/', views.fill_form, name='fill_form'),
     path('enter_data/', views.enter_data, name='enter_data'),
     path('form_submissions/ReviewRecords/<int:submission_id>/', ReviewRecords, name='ReviewRecords'),
+    # for the picture imports
+    path('upload/', views.upload_and_extract, name='upload'),
+    path('result/<int:pk>/', views.result_view, name='result'),
 
-    # User management
+    # User /Patient management
     path('add_user/', views.add_user, name='add_user'),
-
-
+    path('add-patient/', add_patient, name='add_patient'),
+    path('patient-demographics/', views.save_patient_demographics, name='save_patient_demographics'),
 
     # Data operations
     path('import/', import_data, name='import_data'),
@@ -57,7 +62,9 @@ urlpatterns = [
     path('form-submissions/update/<int:submission_id>/', views.update_submission, name='update_submission'),
 
     # API endpoints
-    path('api/patient-by-referral/<str:referral_number>/', get_patient_by_referral, name='get_patient_by_referral'),
+    #path('api/patient-by-referral/<str:referral_number>/', get_patient_by_referral, name='get_patient_by_referral'),
+    path('fetch-patient/', fetch_patient, name='fetch_patient'),
+    path('api/patient-search/', patient_search, name='patient_search'),
     path('api/patient-search/', patient_search, name='patient_search'),
 
     # Bulk operations
